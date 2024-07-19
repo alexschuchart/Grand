4843070161adf0ac0434571486684403823a4469
alexschuchart <alex.schuchart@gmail.com>
2024-07-18 22:59:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
-x = int(input("sup"))
+x = int(input("sup "))
 if x == 7:
     print("yeah")
 else:
     print("uh oh")
 print("july 14 ya dawg")
+
+if x < 8:
+    print("x is", x)
+else:
+    print("rawo")
