5caf1da47159b29415684cb79ae4fc2c125abbf6
alexschuchart <alex.schuchart@gmail.com>
2024-08-14 23:09:34 -0400

    Update Hub.py

--- Hub.py
+++ Hub.py
@@ -1,3 +1,7 @@
 #this will let you navigate to other programs
 
 print("This will be the hub, what would you like to do?")
+
+print("Press 1 for hello")
+
+#i guess you would pick a nubmer here or something?
